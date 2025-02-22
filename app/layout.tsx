b46a3fcae667e82746733bf11be99d3ef66a270b
Ahmed Soliman <ahmadsoliman283@gmail.com>
2025-02-22 07:07:42 +0200

    New Project

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        Nav
         {children}
+        Footer
       </body>
     </html>
   );
